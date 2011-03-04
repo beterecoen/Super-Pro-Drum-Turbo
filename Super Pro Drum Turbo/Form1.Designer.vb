9bd026ab56d08a7cce9a465a5d09c8a81176bb29
unknown <rkoster1@.tudelft.net>
2011-03-04 12:18:38 +0100

    First Commit

---
+++ Super Pro Drum Turbo/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Play = New System.Windows.Forms.Button
+        Me.TabPage3 = New System.Windows.Forms.TabPage
+        Me.TabPage4 = New System.Windows.Forms.TabPage
+        Me.SuspendLayout()
+        '
+        'Play
+        '
+        Me.Play.Location = New System.Drawing.Point(44, 41)
+        Me.Play.Name = "Play"
+        Me.Play.Size = New System.Drawing.Size(75, 23)
+        Me.Play.TabIndex = 0
+        Me.Play.Text = "Play"
+        Me.Play.UseVisualStyleBackColor = True
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(472, 280)
+        Me.TabPage3.TabIndex = 0
+        Me.TabPage3.Text = "TabPage1"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'TabPage4
+        '
+        Me.TabPage4.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage4.Size = New System.Drawing.Size(192, 74)
+        Me.TabPage4.TabIndex = 1
+        Me.TabPage4.Text = "TabPage2"
+        Me.TabPage4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(789, 525)
+        Me.Controls.Add(Me.Play)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Play As System.Windows.Forms.Button
+    Friend WithEvents TabPage3 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage4 As System.Windows.Forms.TabPage
+
+End Class
---
+++ Super Pro Drum Turbo/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports Microsoft.DirectX
+Imports Microsoft.DirectX.DirectDraw
+Imports Microsoft.DirectX.AudioVideoPlayback
+Imports System.Threading
+
+Public Class Form1
+    Dim TrackSample(10) As Audio
+    Dim TrackNotes(10, 64) As Boolean
+    Dim BPM As Integer
+    Dim TrackSpacing As Double
+    Dim TrackPlayThread As New Thread(AddressOf AudioPlay)
+    Dim Playing As Boolean
+    Dim CurrentNoteIndex As Integer
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Play.Click
+        'Voor test
+        BPM = 80
+
+        TrackNotes(1, 0) = True
+        TrackNotes(1, 4) = True
+        TrackNotes(1, 8) = True
+        TrackNotes(1, 12) = True
+        TrackNotes(1, 16) = True
+        TrackNotes(1, 20) = True
+        'Tot hier
+
+        If Playing = True Then
+            Playing = False
+            Play.Text = "Play"
+        Else
+            CurrentNoteIndex = 0
+            TrackSpacing = (1 / (BPM * 4 / 60)) * 1000
+            Playing = True
+            Play.Text = "Stop"
+        End If
+
+    End Sub
+
+    Public Sub AudioPlay()
+        Do While True
+            If Playing Then
+                If TrackNotes(1, CurrentNoteIndex) = True Then
+                    Dim playSound As Audio
+                    playSound = New Audio("h:\Desktop\kick_06.wav", True)
+                End If
+
+                If CurrentNoteIndex < 20 Then
+                    CurrentNoteIndex += 1
+                Else
+                    CurrentNoteIndex = 0
+                End If
+
+                Thread.Sleep(TrackSpacing)
+            End If
+        Loop
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TrackPlayThread.Start()
+    End Sub
+End Class
