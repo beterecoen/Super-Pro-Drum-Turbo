7d37fffa2bb42064036da976a07c8bc75f40b924
RuUb <super_ruup@hotmail.com>
2011-04-12 20:23:59 +0200

    Added Preset Selection Option

--- DrumPro/ControlProperties.vb
+++ DrumPro/ControlProperties.vb
@@ -40,4 +40,89 @@ Public Class ControlProperties
 
     Public Event onBMPChanged()
 
+    Private Property localPresetCollection As New Microsoft.VisualBasic.Collection()
+
+    Public ReadOnly Property PresetCollection As Microsoft.VisualBasic.Collection
+        Get
+            Return localPresetCollection
+        End Get
+    End Property
+
+    Private Property _presetIndex As Integer = 1
+
+    Public Property presetIndex As Integer
+        Set(value As Integer)
+            RaiseEvent onPresetChanged(localPresetCollection.Item(value))
+            _presetIndex = value
+        End Set
+        Get
+            Return _presetIndex
+        End Get
+    End Property
+
+    Public Event onPresetChanged(ByVal SampleCollection As SampleColletion)
+
+    Public ReadOnly Property currentSampleCollection As SampleColletion
+        Get
+            Return localPresetCollection.Item(_presetIndex)
+        End Get
+    End Property
+
+    Public Sub New()
+        localPresetCollection.Add(loadPresetOne())
+        localPresetCollection.Add(loadPresetTwo())
+        localPresetCollection.Add(loadPresetThree())
+    End Sub
+
+    Private Function loadPresetOne()
+        Dim SampleCollection As New SampleColletion
+        SampleCollection.name = "Vinyl"
+        SampleCollection.index = 1
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Vinyl_HiHat.wma", UriKind.Relative), "Vinyl HiHat 1", 1))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Vinyl_HiHat_2.wma", UriKind.Relative), "Vinyl HiHat 2", 2))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Vinyl_open_hihat.wma", UriKind.Relative), "Vinyl open hihat", 3))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Vinyl_Kick.wma", UriKind.Relative), "Vinyl Kick 1", 4))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Vinyl_Kick_2.wma", UriKind.Relative), "Vinyl Kick 2", 5))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Vinyl_Ride.wma", UriKind.Relative), "Vinyl Ride 1", 6))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Vinyl_Ride_2.wma", UriKind.Relative), "Vinyl Ride 2", 7))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Vinyl_Snare.wma", UriKind.Relative), "Vinyl Snare", 8))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Vinyl_Tom_2.wma", UriKind.Relative), "Vinyl Tom 1", 9))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Vinyl_Tom_3.wma", UriKind.Relative), "Vinyl Tom 2", 10))
+        Return SampleCollection
+    End Function
+
+    Private Function loadPresetTwo()
+        Dim SampleCollection As New SampleColletion
+        SampleCollection.name = "FreakyKit"
+        SampleCollection.index = 2
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/FreakyKit Clap.wma", UriKind.Relative), "FreakyKit Clap", 1))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/FreakyKit Crash 1.wma", UriKind.Relative), "FreakyKit Crash 1", 2))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/FreakyKit Crash 2.wma", UriKind.Relative), "FreakyKit Crash 2", 3))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/FreakyKit Kick 1.wma", UriKind.Relative), "FreakyKit Kick 1", 4))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/FreakyKit Kick 2.wma", UriKind.Relative), "FreakyKit Kick 2", 5))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/FreakyKit Perc 1.wma", UriKind.Relative), "FreakyKit Perc 1", 6))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/FreakyKit Perc 2.wma", UriKind.Relative), "FreakyKit Perc 2", 7))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/FreakyKit Perc 3.wma", UriKind.Relative), "FreakyKit Perc 3", 8))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/FreakyKit Perc 4.wma", UriKind.Relative), "FreakyKit Perc 4", 9))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/FreakyKit Perc 5.wma", UriKind.Relative), "FreakyKit Perc 5", 10))
+        Return SampleCollection
+    End Function
+
+    Private Function loadPresetThree()
+        Dim SampleCollection As New SampleColletion
+        SampleCollection.name = "Electro"
+        SampleCollection.index = 3
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Electro Clap 1.wma", UriKind.Relative), "Electro Clap 1", 1))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Electro Clap 2.wma", UriKind.Relative), "Electro Clap 2", 2))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Electro FX 1.wma", UriKind.Relative), "Electro FX 1", 3))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Electro FX 2.wma", UriKind.Relative), "Electro FX 2", 4))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Electro Kick 1.wma", UriKind.Relative), "Electro Kick 1", 5))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Electro Kick 2.wma", UriKind.Relative), "Electro Kick 2", 6))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Electro Kick 3.wma", UriKind.Relative), "Electro Kick 3", 7))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Electro Open HiHat.wma", UriKind.Relative), "Electro Open HiHat", 8))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Electro Snare 1.wma", UriKind.Relative), "Electro Snare 1", 9))
+        SampleCollection.samples.Add(New Sample(New Uri("Samples/Electro Snare 2.wma", UriKind.Relative), "Electro Snare 2", 10))
+        Return SampleCollection
+    End Function
+
 End Class
--- DrumPro/MainPage.xaml.vb
+++ DrumPro/MainPage.xaml.vb
@@ -7,7 +7,7 @@ Partial Public Class MainPage
 
     'Related to the data grid
     Dim NotesPerBeat As Integer = 4
-    Dim NumberOfBeats As Integer = 12
+    Dim NumberOfBeats As Integer = 4
     Dim NumberOfTacks As Integer = 10
 
     'Related to playback
@@ -16,21 +16,23 @@ Partial Public Class MainPage
 
     'Create Tracks Collection
     Dim TrackCollection As New ObservableCollection(Of Track)
-    Dim SampleCollection As New Microsoft.VisualBasic.Collection()
     Dim mediaElementContainer As New Canvas
     Dim ControlPropertiesObject As New ControlProperties()
     Dim AudioTimer As New System.Windows.Threading.DispatcherTimer()
 
     Public Sub New()
         InitializeComponent()
-        InitializeSamples()
         InitializeTracks()
         UpdateAudioSpacing()
         LayoutRoot.Children.Add(mediaElementContainer)
         mediaElementContainer.Children.Add(New MediaElement)
+
         MasterVolumeSilder.DataContext = ControlPropertiesObject
         BMPControl.DataContext = ControlPropertiesObject
+        PresetSelection.DataContext = ControlPropertiesObject
+
         AddHandler ControlPropertiesObject.onBMPChanged, AddressOf UpdateAudioSpacing
+        AddHandler ControlPropertiesObject.onPresetChanged, AddressOf UpdateSampleCollection
         AddHandler AudioTimer.Tick, AddressOf PlayColumnSamples
     End Sub
 
@@ -38,13 +40,18 @@ Partial Public Class MainPage
         AudioTimer.Interval = New TimeSpan((1 / (ControlPropertiesObject.BPM * 4 / 60) * 10000000))
     End Sub
 
+    Sub UpdateSampleCollection(ByVal SampleCollection As SampleColletion)
+        For Each track As Track In TrackCollection
+            track.sampleOptions = SampleCollection.samples
+        Next
+    End Sub
 
     'Function to initialize the Tracks
     Sub InitializeTracks()
         For trackIndex As Integer = 0 To NumberOfTacks - 1
             Dim track As New Track
             track.volume = 0.3
-            track.sampleOptions = SampleCollection
+            track.sampleOptions = ControlPropertiesObject.currentSampleCollection.samples
             track.sampleIndex = trackIndex + 1
 
             For beatIndex As Integer = 0 To NumberOfBeats - 1
@@ -63,42 +70,6 @@ Partial Public Class MainPage
         TrackControlsPanel.DataContext = TrackCollection
     End Sub
 
-    Sub InitializeSamples2()
-        SampleCollection.Add(New Sample(New Uri("Samples/Vinyl_HiHat.wMA", UriKind.Relative), "Vinyl HiHat 1", 1))
-        SampleCollection.Add(New Sample(New Uri("Samples/Vinyl_HiHat_2.wma", UriKind.Relative), "Vinyl HiHat 2", 2))
-        SampleCollection.Add(New Sample(New Uri("Samples/Vinyl_open_hihat.wma", UriKind.Relative), "Vinyl open hihat", 3))
-        SampleCollection.Add(New Sample(New Uri("Samples/Vinyl_Kick.wma", UriKind.Relative), "Vinyl Kick 1", 4))
-        SampleCollection.Add(New Sample(New Uri("Samples/Vinyl_Kick_2.wma", UriKind.Relative), "Vinyl Kick 2", 5))
-        SampleCollection.Add(New Sample(New Uri("Samples/Vinyl_Ride.wma", UriKind.Relative), "Vinyl Ride 1", 6))
-        SampleCollection.Add(New Sample(New Uri("Samples/Vinyl_Ride_2.wma", UriKind.Relative), "Vinyl Ride 2", 7))
-        SampleCollection.Add(New Sample(New Uri("Samples/Vinyl_Snare.wma", UriKind.Relative), "Vinyl Snare", 8))
-        SampleCollection.Add(New Sample(New Uri("Samples/Vinyl_Tom_2.wma", UriKind.Relative), "Vinyl Tom 1", 9))
-        SampleCollection.Add(New Sample(New Uri("Samples/Vinyl_Tom_3.wma", UriKind.Relative), "Vinyl Tom 2", 10))
-    End Sub
-    Sub InitializeSamples3()
-        SampleCollection.Add(New Sample(New Uri("Samples/FreakyKit Clap.wma", UriKind.Relative), "FreakyKit Clap", 1))
-        SampleCollection.Add(New Sample(New Uri("Samples/FreakyKit Crash 1.wma", UriKind.Relative), "FreakyKit Crash 1", 2))
-        SampleCollection.Add(New Sample(New Uri("Samples/FreakyKit Crash 2.wma", UriKind.Relative), "FreakyKit Crash 2", 3))
-        SampleCollection.Add(New Sample(New Uri("Samples/FreakyKit Kick 1.wma", UriKind.Relative), "FreakyKit Kick 1", 4))
-        SampleCollection.Add(New Sample(New Uri("Samples/FreakyKit Kick 2.wma", UriKind.Relative), "FreakyKit Kick 2", 5))
-        SampleCollection.Add(New Sample(New Uri("Samples/FreakyKit Perc 1.wma", UriKind.Relative), "FreakyKit Perc 1", 6))
-        SampleCollection.Add(New Sample(New Uri("Samples/FreakyKit Perc 2.wma", UriKind.Relative), "FreakyKit Perc 2", 7))
-        SampleCollection.Add(New Sample(New Uri("Samples/FreakyKit Perc 3.wma", UriKind.Relative), "FreakyKit Perc 3", 8))
-        SampleCollection.Add(New Sample(New Uri("Samples/FreakyKit Perc 4.wma", UriKind.Relative), "FreakyKit Perc 4", 9))
-        SampleCollection.Add(New Sample(New Uri("Samples/FreakyKit Perc 5.wma", UriKind.Relative), "FreakyKit Perc 5", 10))
-    End Sub
-    Sub InitializeSamples()
-        SampleCollection.Add(New Sample(New Uri("Samples/Electro Clap 1.wma", UriKind.Relative), "Electro Clap 1", 1))
-        SampleCollection.Add(New Sample(New Uri("Samples/Electro Clap 2.wma", UriKind.Relative), "Electro Clap 2", 2))
-        SampleCollection.Add(New Sample(New Uri("Samples/Electro FX 1.wma", UriKind.Relative), "Electro FX 1", 3))
-        SampleCollection.Add(New Sample(New Uri("Samples/Electro FX 2.wma", UriKind.Relative), "Electro FX 2", 4))
-        SampleCollection.Add(New Sample(New Uri("Samples/Electro Kick 1.wma", UriKind.Relative), "Electro Kick 1", 5))
-        SampleCollection.Add(New Sample(New Uri("Samples/Electro Kick 2.wma", UriKind.Relative), "Electro Kick 2", 6))
-        SampleCollection.Add(New Sample(New Uri("Samples/Electro Kick 3.wma", UriKind.Relative), "Electro Kick 3", 7))
-        SampleCollection.Add(New Sample(New Uri("Samples/Electro Open HiHat.wma", UriKind.Relative), "Electro Open HiHat", 8))
-        SampleCollection.Add(New Sample(New Uri("Samples/Electro Snare 1.wma", UriKind.Relative), "Electro Snare 1", 9))
-        SampleCollection.Add(New Sample(New Uri("Samples/Electro Snare 2.wma", UriKind.Relative), "Electro Snare 2", 10))
-    End Sub
     'The click callback on the Play/Stop button
     Private Sub Play_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Play.Click
         CurrentPlayIndex = 0
@@ -122,16 +93,15 @@ Partial Public Class MainPage
     'Function to play the samples of the current column
     Public Sub PlayColumnSamples()
         'Go through all the tracks
-        For trackIndex As Integer = 0 To NumberOfTacks - 1
-            Dim track As Track = TrackCollection.Item(trackIndex)
+        For Each track As Track In TrackCollection
             If track.isNoteChecked(CurrentPlayIndex) Then
                 PlaySample(track.getSample, track.volume)
             End If
-            track.HighlightCurrent()
-            UpdateScollPosition()
+            'track.HighlightCurrent()
         Next
-        For trackIndex As Integer = 0 To NumberOfTacks - 1
-            TrackCollection.Item(trackIndex).loadNextSample()
+        'UpdateScollPosition()
+        For Each track As Track In TrackCollection
+            track.loadNextSample()
         Next
         incrementPlayIndex()
     End Sub
